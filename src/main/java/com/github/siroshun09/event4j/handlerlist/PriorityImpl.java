44cb5e0429d5a21b3fcdd4ef78b1c7a81137029f
Siroshun09 <siro.shun8@gmail.com>
2020-12-03 04:06:34 +0900

    [Add] PriorityImpl

---
+++ src/main/java/com/github/siroshun09/event4j/handlerlist/PriorityImpl.java
@@ -0,0 +1,67 @@
+/*
+ *     Copyright (c) 2020 Siroshun09
+ *
+ *     This file is part of Event4J.
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *     SOFTWARE.
+ */
+
+package com.github.siroshun09.event4j.handlerlist;
+
+import java.util.Objects;
+
+class PriorityImpl implements Priority {
+
+    private final int value;
+
+    PriorityImpl(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int getPriority() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o instanceof Priority) {
+            Priority priority = (Priority) o;
+            return value == priority.getPriority();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "PriorityImpl{" +
+                "value=" + value +
+                '}';
+    }
+}
